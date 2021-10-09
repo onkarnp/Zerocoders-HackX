b26665371399f0451b6f8c2638db9c3ba80d2775
onkarnp <opanchare123@gmail.com>
2021-10-09 22:29:10 +0530

    Authentication done.

--- app/src/main/java/com/example/zerocoders/log_in_page.java
+++ app/src/main/java/com/example/zerocoders/log_in_page.java
@@ -1,10 +1,25 @@
 package com.example.zerocoders;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Objects;
 
@@ -14,6 +29,11 @@ public class log_in_page extends AppCompatActivity {
 
     //    Initializing blurrkit
     BlurLayout blurLayout;
+    private EditText email,password;
+    CardView forgotPassword, signincard, signupcard;
+    private FirebaseAuth mAuth;
+    private ProgressDialog loadingBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +42,110 @@ public class log_in_page extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();    //removes action bar
         blurLayout = findViewById(R.id.blurLayout);         //for blurring background
         setContentView(R.layout.activity_log_in_page);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        loadingBar = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+//        if(mAuth.getCurrentUser() != null){
+//            startActivity(new Intent(log_in_page.this,homepage.class));
+//            finish();
+//        }
+
+
+        signupcard = (CardView) findViewById(R.id.signupcard);
+        signupcard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), sign_up_page.class);
+                startActivity(intent);
+            }
+        });
+
+        signincard = (CardView) findViewById(R.id.signincard);
+        signincard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logInUser();
+            }
+        });
+
+        forgotPassword = (CardView) findViewById(R.id.forgotpassword);
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText resetMail = new EditText(v.getContext());
+                AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Reset Password ?");
+                passwordResetDialog.setMessage("Enter your email to receive reset link.");
+                passwordResetDialog.setView(resetMail);
+
+                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //extract the mail and send reset link
+
+                        String mail = resetMail.getText().toString();
+                        mAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(log_in_page.this, "Reset link has been sent to your mail :)", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(log_in_page.this, "Error :( Reset link is not sent." + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //closing dialogue
+                    }
+                });
+                passwordResetDialog.create().show();
+            }
+        });
+    }
+
+    public void logInUser() {
+        String Email = email.getText().toString();
+        String Password = password.getText().toString();
+
+        if (!Email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(Email).matches()) {
+            if (!Password.isEmpty()) {
+                loadingBar.setTitle("Logging in");
+                loadingBar.setMessage("Checking credentials...");
+                loadingBar.setCanceledOnTouchOutside(false);
+                loadingBar.show();
+                mAuth.signInWithEmailAndPassword(Email,Password)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast.makeText(log_in_page.this, "Logged in successfully !!", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                                startActivity(new Intent(log_in_page.this,homepage.class));
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        loadingBar.dismiss();
+                        Toast.makeText(log_in_page.this, "Log in failed :(", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } else {
+                showError(password, "Empty fields are not allowed.");
+            }
+        } else if(Email.isEmpty())
+        {
+            showError(email,"Empty fields are not allowed.");
+        }
+        else{
+            showError(email,"Please Enter correct Email.");
+        }
     }
 
     //Functions for making background blur (needed to be included to run blurrkit)
@@ -37,5 +161,12 @@ public class log_in_page extends AppCompatActivity {
     protected void onStop() {
         blurLayout.pauseBlur();
         super.onStop();
+    }
+
+    //Function to show error message when input is not in correct format
+    public void showError(EditText input, String s)
+    {
+        input.setError(s);
+        input.requestFocus();
     }
 }
